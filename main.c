6cf3d413e2f009a4e54190d045c014c41f778340
Enzo Danjour <enzo.lops999@gmail.com>
2020-12-11 11:28:56 -0300

    Add printfs

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
  
 int main(void) {
     Dados *d1, *d2, *d3;
+    printf("Hello, welcome, select a scenario to model:\n");
+    printf("1° Scenario: Nothing is done\n");
+    printf("2° Scenario: Distancing and wear masks\n");
+    printf("3° Scenario: Improvement in service protocols\n");
+    printf("4° Scenario: Distancing, wear masks and improvment in service protocols\n");
     simulacaoSIR(d1, d2, d3);
  
     return 0;

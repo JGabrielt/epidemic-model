2e7d6b327f75c70dd7e152e998ff406e21f229dc
Enzo Danjour <enzo.lops999@gmail.com>
2020-12-11 11:37:41 -0300

    Add input example

--- main.c
+++ main.c
@@ -4,9 +4,10 @@ int main(void) {
     Dados* d;
     int cenario;
     printf("Hello, welcome, select a scenario to model:\n");
-    printf("1° Scenario: Nothing is done\n");
-    printf("2° Scenario: Distancing and wear masks\n");
-    printf("3° Scenario: Improvement in service protocols\n");
+    printf("1: Nothing is done\n");
+    printf("2: Distancing and wear masks\n");
+    printf("3: Improvement in service protocols\n");
+    printf("input example: 1\n");
     scanf("%d", &cenario);
     simulacaoSIR(d, cenario);
  
